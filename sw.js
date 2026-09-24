e3f7016f94e4c8e33a712e0c6a30ab31ed8cfdb6
mverapol-netizen <mvera.pol@gmail.com>
2026-09-24 01:11:30 -0300

    Bump offline cache for v0.5

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='vri5000-lab-v04';
+const CACHE='vri5000-lab-v05';
 const ASSETS=[
   './','./index.html','./styles.css','./manifest.webmanifest','./assets/icon.svg',
   './js/app.js','./js/content.js','./js/store.js','./js/engine.js'
