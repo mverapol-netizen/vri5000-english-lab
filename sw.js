7d4399575785698f9329294dc451fe8f5974453c
mverapol-netizen <mvera.pol@gmail.com>
2026-09-24 01:48:51 -0300

    Refresh offline cache for v0.9

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='vri5000-lab-v08';
+const CACHE='vri5000-lab-v09';
 const ASSETS=[
   './','./index.html','./styles.css','./manifest.webmanifest','./assets/icon.svg',
   './js/app.js','./js/content.js','./js/store.js','./js/engine.js'
