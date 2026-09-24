932773f7252694c9d863a3f9e743bdb523ef72c3
mverapol-netizen <mvera.pol@gmail.com>
2026-09-24 01:50:41 -0300

    Refresh offline cache after v0.9 fluency expansion

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='vri5000-lab-v09';
+const CACHE='vri5000-lab-v091';
 const ASSETS=[
   './','./index.html','./styles.css','./manifest.webmanifest','./assets/icon.svg',
   './js/app.js','./js/content.js','./js/store.js','./js/engine.js'
